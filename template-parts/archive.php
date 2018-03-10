94c3ec3492038142c01569b0c656a14ecc8fb72a
Manuel Schmalstieg <git.code@ms-studio.net>
2018-03-10 18:04:22 +0100

    Test if Minimalistic Event Manager is loaded before using the function.

--- template-parts/archive.php
+++ template-parts/archive.php
@@ -23,10 +23,14 @@
 			// Check Date!
 			$current_post_id = get_the_ID();
 			
-			$mem_date = mem_date_processing( 
-				get_post_meta($current_post_id, '_mem_start_date', true) , 
-				get_post_meta($current_post_id, '_mem_end_date', true)
-			);
+			if ( function_exists('mem_date_processing') ) {
+			
+				$mem_date = mem_date_processing( 
+					get_post_meta($current_post_id, '_mem_start_date', true) , 
+					get_post_meta($current_post_id, '_mem_end_date', true)
+				);
+			
+			}
 			
 			if ($mem_date["start-iso"] !="" ) { 
 				
