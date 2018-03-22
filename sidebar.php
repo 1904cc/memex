fc60e6e774dd17cbfd704cac59e5a035abb7724f
Manuel Schmalstieg <git.code@ms-studio.net>
2018-03-22 22:21:53 +0100

    Disabling the sidebar

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+<?php
+
+// Disabling Sidebar
+
+// <aside id="secondary" class="widget-area">
+// 	<?php dynamic_sidebar( 'sidebar-1' );
+// </aside><!-- #secondary -->
